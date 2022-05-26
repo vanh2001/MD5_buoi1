66902d5c1db2f16e2f2a3e2c886bc557c5687016
vanh2001 <anhviet1709@gmail.com>
2022-05-26 15:38:29 +0700

    Add file TH5

---
+++ typescript-promise/dist/main.js
@@ -0,0 +1,20 @@
+let money = 1000;
+const buyACar = (car) => {
+    return new Promise(((resolve, reject) => {
+        setTimeout(() => {
+            if (money >= 10000) {
+                resolve("can buy" + car);
+            }
+            else {
+                reject("Do not enough money");
+            }
+        }, 100);
+    }));
+};
+money = 1;
+const promise = buyACar("vinfast").then(value => {
+    console.log(value);
+}, error => {
+    console.log(error);
+});
+//# sourceMappingURL=main.js.map
